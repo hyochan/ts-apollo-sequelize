9ef392e72d9803020ca1f62a763b0f7803ae08f0
hyochan <dooboolab@gmail.com>
2019-05-01 03:12:11 +0900

    Setup jwt auth
    - Set jwt as a middleware of graphql-yoga. Renamed queries so it looks like talktalk.

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,22 @@
 import * as path from 'path';
 import { GraphQLServer } from 'graphql-yoga';
 import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
+import authMiddleware from './middlewares/authMiddleware';
+
+const {
+  PORT = 4000,
+  DEBUG = 'false',
+  JWT_SECRET = 'undefined',
+} = process.env;
 
 const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './resolvers')));
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
+  middlewares: [authMiddleware(JWT_SECRET)],
+  context: (req) => ({
+    ...req,
+  }),
   resolvers: resolvers,
 });
 
